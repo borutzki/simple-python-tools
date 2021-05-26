aa0ba7569023ad1a78ca95cf3e7a2e65389221fe
Kacper Borucki <kacper.marcin.borucki@gmail.com>
2021-05-26 19:15:51 +0200

    2021-05-26 working version of split_pd

--- split_pdf/split_pdf.py
+++ split_pdf/split_pdf.py
@@ -1,38 +1,48 @@
-import PyPDF2 
+import PyPDF2
 import tkinter as tk
 from tkinter import filedialog
+import ntpath
+
 
 def get_pages(pdf_path):
+    """
+    Function obtaining pages as an objects and returning them as an array.
+    """
     pdf_file = PyPDF2.PdfFileReader(pdf_path)
-    print(pdf_file.documentInfo)
-    
-    pages = []
-    for i in range(0,pdf_file.numPages):
-        page = pdf_file.getPage(i)
-        pages.append(page)
+    pages = [pdf_file.getPage(i) for i in range(0, pdf_file.numPages)]
+
     return pages
 
-def make_files(pages,output_directory):
-    
-    for i in range(0,len(pages)):
+
+def make_files(pages, file_name, output_directory):
+    """
+    Function that takes PyPDF2 pages objects and exports them as PDF files.
+    """
+    for i in range(0, len(pages)):
+        output_path = output_directory + "/" + file_name + f"_{i+1}" + ".pdf"
 
         pdf_creator = PyPDF2.PdfFileWriter()
         pdf_creator.addPage(pages[i])
-        pdf_output = open(output_directory,"wb")
+
+        pdf_output = open(output_path, "wb")
         pdf_creator.write(pdf_output)
+        pdf_output.close()
+
 
 def get_file_paths():
-    
+    """
+    Function which obtains paths of files from user input.
+    """
     root = tk.Tk()
     root.withdraw()
-    
-    file_paths = filedialog.askopenfilenames(filetypes=[("PDF File","*.pdf")])
-
+    file_paths = filedialog.askopenfilenames(filetypes=[("PDF File", "*.pdf")])
     return file_paths
 
 
 def get_directory():
-    print("Choose output folder")
+    """
+    Function which obtains output folder path from user input.
+    """
     root = tk.Tk()
     root.withdraw()
 
@@ -40,16 +50,21 @@ def get_directory():
     return directory
 
 
+"""
+Here starts the script
+"""
 
-# fp = file_paths[0].replace(".pdf",f"{1}.pdf")
-# print(fp)
-
+print("Select files to split")
 file_paths = get_file_paths()
+print("Choose output folder")
+output_directory = get_directory()
 
-if file_paths != "":
-    output_directory = get_directory()
-    
+if file_paths != "" or output_directory != "":
+
     for i in range(len(file_paths)):
-        
+        file_name = ntpath.basename(file_paths[i]).replace(".pdf", "")
         pages = get_pages(file_paths[i])
-        make_files(pages,output_directory)
+        make_files(pages, file_name, output_directory)
+
+else:
+    print("No files or output path selected!")
